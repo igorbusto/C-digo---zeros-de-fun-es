b84445ba64579b40b539bdd34fb624209eee28f6
thomassolcia <thomas.nascimento@edu.unifil.br>
2020-09-18 01:22:11 -0300

    Integração da interface

--- Bissecao.py
+++ Bissecao.py
@@ -1,27 +1,51 @@
-def func(x): 
-    return 10 - x*x
+from math import *
+import numpy as np
+import PySimpleGUI as sg
 
-def bisection(a,b): 
-  
-    if (func(a) * func(b) >= 0): 
-        print("Você não inseriu valores válidos para a ou b\n") 
-        return
-   
-    c = a 
-    while ((b-a) >= 0.01): 
-        # Ponto médio
-        c = (a+b)/2
-        # Checar se ponto médio é a raiz
-        if (func(c) == 0.0): 
-            break
-        # Deicidir o lado da repeitção (+/-)
-        if (func(c)*func(a) < 0): 
-            b = c 
-        else: 
-            a = c 
-    print("O valor da raiz é:","%.4f"%c) 
+class TelaPython:
+    def __init__(self):
+        # layout
+        layout = [
+            [sg.Text('f(x)'), sg.Input(size=(15,0))],
+            [sg.Text('a(+)'), sg.Input(size=(3,0))],
+            [sg.Text('b(-)'), sg.Input(size=(3,0))],
+            [sg.Button('Executar'), sg.Button('Limpar')],
+            [sg.Text('Resultado')],
+            [sg.Output(size=(40,15))]
+        ]
+        # Janela
+        self.janela = sg.Window("Somatório PF").layout(layout)
+    def Iniciar(self):
+        while True:
+            # Extrair dados da tela
+            self.button, self.values= self.janela.Read()
+            def func(expr, x):
+                return eval(expr)
 
-# Valores iniciais
-a = -2
-b = 5
-bisection(a, b) 
+            input_expr = self.values[0]
+            a = eval(self.values[1])
+            b = eval(self.values[2])
+
+            def bisection(a,b): 
+                if (func(input_expr, a) * func(input_expr, b) >= 0): 
+                    print("Você não inseriu valores válidos para a ou b\n") 
+                    return
+                c = a 
+                iteracoes = 0
+                while ((b-a) >= 0.01): 
+                    iteracoes += 1
+                    # Ponto médio
+                    c = (a+b)/2
+                    # Checar se ponto médio é a raiz
+                    if (func(input_expr,c) == 0.0): 
+                        break
+                    # Deicidir o lado da repeitção (+/-)
+                    if (func(input_expr,c)*func(input_expr,a) < 0): 
+                        b = c 
+                    else: 
+                        a = c 
+                print("Raiz:","%.4f"%c) 
+                print("Iterações:", iteracoes)
+            bisection(a, b)
+tela = TelaPython()
+tela.Iniciar()
