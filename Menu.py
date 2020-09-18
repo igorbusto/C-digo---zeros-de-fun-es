9f88e3dee938f355e1cb4245470eb65e0711e6ce
thomassolcia <thomas.nascimento@edu.unifil.br>
2020-09-18 15:10:29 -0300

    Adicionado um menu de métodos

---
+++ Menu.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as sg
+
+class TelaMenu:
+    sg.theme('Light Blue 3')
+
+    def button1(self=None):
+        from Metodos.Bissecao import TelaBissecao
+        TelaBissecao().Iniciar()
+
+    def button2(self=None):
+        print('Newton')
+
+    dispatch_dictionary = {'Bisseção':button1, 'Newton':button2}
+    layout = [[sg.Text('Selecione um método', auto_size_text=True)],
+            [sg.Button('Bisseção'), sg.Button('Newton'), sg.Quit('Encerrar')]]
+
+    window = sg.Window('Métodos', layout)
+    while True:
+        event, value = window.read()
+        if event in ('Encerrar', sg.WIN_CLOSED):
+            break
+        if event in dispatch_dictionary:
+            func_to_call = dispatch_dictionary[event]
+            func_to_call()
+        else:
+            print('Event {} não encontrado'.format(event))
+
+    window.close()
+    sg.popup_ok('Menu finalizado!')
+menu = TelaMenu()
